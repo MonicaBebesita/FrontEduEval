d60b8291c9915f3e0f762f6d702229044ec7b196
DDarkP <danielfelipenov@gmail.com>
2025-06-05 05:32:20 -0500

    Mensajes en login con Toastify

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -4,6 +4,8 @@ import { FormsModule } from '@angular/forms';
 import { Router, RouterModule } from '@angular/router';
 import { IconComponent } from '../components/atoms/icon/icon.component';
 import { AuthService } from '../services/seguridad/auth.service';
+import Toastify from 'toastify-js';
+import "toastify-js/src/toastify.css";
 
 @Component({
   selector: 'app-login',
@@ -21,32 +23,46 @@ export class LoginComponent {
 
   onSubmit() {
     console.log('[LoginComponent] Intentando iniciar sesión...');
-    console.log('[LoginComponent] Usuario:', this.username); // ¡Cuidado con mostrar la contraseña en logs de producción!
+    console.log('[LoginComponent] Usuario:', this.username);
 
     if (!this.username || !this.password) {
       this.error = 'Por favor ingrese usuario y contraseña.';
       console.error('[LoginComponent] Error: Usuario o contraseña vacíos.');
+      this.showToast(this.error, 'linear-gradient(to right, #ff5f6d, #ffc371)');
       return;
     }
 
     this.auth.login(this.username, this.password).subscribe({
       next: () => {
         this.error = null;
-        console.log(
-          '[LoginComponent] Login exitoso. Redirigiendo a /dashboard.'
-        );
+        this.showToast('¡Inicio de sesión exitoso!', 'linear-gradient(to right, #00b09b, #96c93d)');
+        console.log('[LoginComponent] Login exitoso. Redirigiendo a /dashboard.');
         this.router.navigate(['/dashboard']);
       },
       error: (err) => {
         this.error = 'Credenciales incorrectas.';
-        console.error('[LoginComponent] Error en el login:', err); // Loggea el error completo para depuración
+        this.showToast(this.error, 'linear-gradient(to right, #ff5f6d, #ffc371)');
+        console.error('[LoginComponent] Error en el login:', err);
       },
     });
   }
-  // Método para manejar el cambio en los campos de entrada. Resetea el error si el usuario comienza a escribir
+
   onInputChange() {
     if (this.error) {
       this.error = null;
     }
+  }
+
+  // Método para lanzar toasts
+  private showToast(message: string, background: string) {
+    Toastify({
+      text: message,
+      duration: 3000,
+      close: true,
+      gravity: 'top',
+      position: 'right',
+      backgroundColor: background,
+      stopOnFocus: true,
+    }).showToast();
   }
 }
